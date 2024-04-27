fd92bf12ce11ab285b986cc7e2b6c5ed3c6a3054
Sam Elijah <samelijah85@gmail.com>
2024-04-27 17:36:05 +0200

    State route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -45,11 +45,11 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Create a new State."""
-    data = request.get_json()
-    if not data:
+    if not request.get_json():
         abort(400, "Not a JSON")
-    if 'name' not in data:
+    if 'name' not in request.get_json():
         abort(400, "Missing name")
+    data = request.get_json()
     state = State(**data)
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
@@ -61,9 +61,9 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    data = request.get_json()
-    if not data:
+    if not request.get_json():
         abort(400, "Not a JSON")
+    data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
