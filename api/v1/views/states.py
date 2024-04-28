b7a7649a54442767bdb2bbf21dd0ab6015379e32
Sam Elijah <samelijah85@gmail.com>
2024-04-29 00:12:46 +0200

    Updated error handling in states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,6 +5,7 @@ Module: api/vi/views/states.py
 API endpoints for State objects.
 """
 from flask import abort, jsonify, make_response, request
+from werkzeug.exceptions import BadRequest
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -39,17 +40,20 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return make_response(jsonify({}), 200)
+    return jsonify({})
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Create a new State."""
-    if not request.get_json():
-        abort(400, "Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, "Missing name")
-    data = request.get_json()
+    if not request.is_json:
+        return make_response(jsonify({'error': "Not a JSON"}), 400)
+    try:
+        data = request.get_json()
+    except BadRequest:
+        return make_response(jsonify({'error': "Not a JSON"}), 400)
+    if 'name' not in data:
+        return make_response(jsonify({'error': "Missing name"}), 400)
     state = State(**data)
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
@@ -61,11 +65,14 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    if not request.get_json():
-        abort(400, "Not a JSON")
-    data = request.get_json()
+    if not request.is_json:
+        return make_response(jsonify({'error': "Not a JSON"}), 400)
+    try:
+        data = request.get_json()
+    except BadRequest:
+        return make_response(jsonify({'error': "Not a JSON"}), 400)
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return jsonify(state.to_dict())
